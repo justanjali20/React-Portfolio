cd8a3ad7824cf210428c3b861c4205b78025335b
Anjali Pandey <justanjali20@gmail.com>
2024-06-07 21:56:08 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,301 @@
+import Calculator from '../Assets/Calculator.png';
+import ECom from '../Assets/ECom.png';
+import news from '../Assets/news.png';
+import Restro from '../Assets/Restro.png';
+import Rportfolio from '../Assets/Rportfolio.png';
+import todo from '../Assets/todo.png';
+import weather from '../Assets/weather.png';
+import Yportfolio from '../Assets/Yportfolio.png';
+
+export const Bio = {
+  name: "Anjali Pandey",
+  roles: [
+    "Front-end Developer",
+    "React Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/justanjali20",
+  resume:
+    "https://drive.google.com/file/d/1DD-8qXHZOXF-kLnREamMBRS5pOXc2-TP/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/anjali-pandey-48b438256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/justanjali.?igsh=MW82YmdmZmlvNjE2MQ==",
+  facebook: "https://www.facebook.com/profile.php?id=100011561623115&mibextid=rS40aB7S9Ucbxw6v",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+    
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+  
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4D0BAQFVBPbc6-yxoA/company-logo_200_200/0/1677167041761?e=1724889600&v=beta&t=4N0sNdHRwLVY2gU679O79yOp3rf7G60OS92a-yQ-p6U",
+    role: "Front-end developer Intern",
+    company: "CodeAlpha Pvt. Ltd.",
+    date: "April 2024 -  June 2024",
+    desc: "Working on HTML, CSS, JavaScript, frameworks such as React , and server-side scripting..",
+    skills: [
+      "JavaScript",
+      "React",
+      "HTML",
+      "Css"
+    ],
+  },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQHDmIfYA5sY7w/company-logo_200_200/0/1685777115831?e=1724889600&v=beta&t=47FVGfPVqeD2XCi8MXLct-L-tLpk5m_ZmgNykk_cR3U",
+    role: "Frontend Engineer Intern",
+    company: "CodeSoft",
+    date: "Jan 2024  - Feb 2024",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4E0BAQHfk_O8amPovQ/company-logo_200_200/0/1642532597684/oasis_infobyte_logo?e=2147483647&v=beta&t=YpWhXqvJ6rPztP0uXz7ZnXw4qgmNbrMGAUoBsN1yVsw",
+    role: "Web developer intern",
+    company: "Oasis Infobyte",
+    date: "Feb 2024 - Jan 2024",
+    desc: "Contributing to different open-source projects ",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Axios"
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C4D0BAQFheEbgt4_U_w/company-logo_200_200/0/1661277231688/codeclause_logo?e=2147483647&v=beta&t=oaLfv8BROAFUk-_O81fbBJOU2aw-Mlgx_tvPWgVy3EE",
+    role: "Front-end developer intern ",
+    company: "CodeClause",
+    date: "Oct 2023 - Nov 2023",
+    desc: "Working on HTML, CSS, JavaScript, frameworks such as React",
+    
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/D4D0BAQFoc4yAVXihdA/company-logo_200_200/0/1694328236668?e=2147483647&v=beta&t=R0Zb2HrweKGWxAtyDYYmbpA0dXYjTZ91P6d6QqvcSj4",
+    role: "Web Developer",
+    company: "Internsavy",
+    date: "May 2023 - June2023",
+    desc: "As an Web developer at the Internsavy, I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology.",
+    skills: [
+      "Wordpress", 
+      "Figma",
+      "HTML",
+      "CSS",
+    ],
+  },
+  
+];
+
+
+
+export const projects = [
+  {
+    id: 9,
+    title: "Calculator",
+    date: "Jan 2024 - feb 2024",
+    description:
+      "The React Calculator is a versatile and user-friendly web application designed to perform basic arithmetic operations.Whether you need to add, subtract, multiply, or divide, this calculator has you covered with accurate and instant results.",
+    image:
+    Calculator,
+    tags: [
+      "React Js",
+      "Javascript",
+      "HTML",
+      "CSS",
+    ],
+    category: "web app",
+    github: "https://github.com/justanjali20/my-calculator",
+    webapp: "https://justanjali20.github.io/my-calculator/",
+  },
+  {
+    id: 0,
+    title: "Portfolio",
+    date: "Nov 2023 - Feb 2024",
+    description:
+      "Built with React, this site showcases my skills and experience in web development. Explore my projects and get in touch!",
+    image:
+    Rportfolio,
+    tags: ["React Js", "Javascript", "CSS", "HTML", "React-dom"],
+    category: "web app",
+    github: "https://github.com/justanjali20/portfolio",
+    webapp: "https://justanjali20.github.io/portfolio/",
+   
+  },
+  {
+    id: 1,
+    title: "Portfolio",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "This React-powered portfolio is a glimpse into my skills and design philosophy.",
+    image:
+    Yportfolio ,
+    tags: [
+      "React Js",
+      "emailjs",
+      "Javascript",
+      "HTML",
+      "CSS",
+      "react-router-dom",
+      "react-scroll"
+
+    ],
+    category: "web app",
+    github: "https://github.com/justanjali20/My-portfolio",
+    webapp: "https://subtle-paletas-3921be.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Todo-list",
+    date: "Feb 2024 - Feb 2024",
+    description:
+      "Built with React, this application ensures a seamless and dynamic user experience, making task management intuitive and straightforward",
+    image:
+     todo,
+    tags: ["Javascript", "React", "HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/justanjali20/Todo-list",
+    webapp: "https://justanjali20.github.io/Todo-list/",
+    
+  },
+  {
+    id: 3,
+    title: "ECOM-Site",
+    date: "Mar 2024 - Apr 2024",
+    description:
+      "Premier Footwear E-Commerce Website, your ultimate destination for the latest in shoe fashion, Provides Responsive Design, User-Friendly Interface and Personalized Experience",
+    image:
+      ECom,
+    tags: ["API", "React", "Javascript", "CSS", "HTML", "redux"],
+    category: "web app",
+    github: "https://github.com/justanjali20/Ecomsite",
+    webapp: "https://justanjali20.github.io/Ecomsite/",
+  },
+  {
+    id: 10,
+    title: "News App",
+    date: "March 2024 - March 2024",
+    description:
+      "A News Finding App made with React, Axios. Users can search for any news coming from API and watch it.",
+    image:
+      news,
+    tags: ["React", "JavaScript", "Axios", "HTML", "CSS"],
+    category: "web app",
+    github: "https://github.com/justanjali20/newsapp",
+    webapp: "https://justanjali20.github.io/newsapp/",
+  },
+  {
+    id: 4,
+    title: "Restro App",
+    date: "April 2024",
+    description:
+      "A Restro App with React JS, Javascript, HTML, CSS, React Restaurant App, designed to elevate your dining experience with modern convenience and seamless functionality",
+    image:
+      Restro,
+    tags: ["React Js", "Javascript", "CSS", "HTML"],
+    category: "web app",
+    github: "https://github.com/justanjali20/restaurant-site",
+    webapp: "https://justanjali20.github.io/restaurant-site/",
+  },
+  {
+    id: 5,
+    title: "Weather App",
+    date: "Sep 2023",
+    description:
+      "React Weather App, your go-to solution for real-time weather updates and forecasts.",
+    image:
+      weather,
+    tags: ["React Js", "Local Storage"],
+    category: "web app",
+    github: "https://github.com/justanjali20/weather-app",
+    webapp: "https://justanjali20.github.io/weather-app/",
+  },
+  
+];
+
+
